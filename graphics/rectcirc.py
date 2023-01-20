4c06dfda8a8e26b81d55ea504e3b7be3fc65d513
psvishnu02 <116252680+psvishnu02@users.noreply.github.com>
2023-01-20 15:04:51 +0530

    Add files via upload

---
+++ graphics/rectcirc.py
@@ -0,0 +1,9 @@
+def rectarea (l,b):
+    print("Area of Rectangle: ",l*b)
+def rectperi (l,b):
+    print("Perimeter of rectangle: ",2*(l+b))
+
+def circlearea (r):
+    print("Area of Circle: ",3.14*r*r)
+def circleperi (r):
+    print("Perimeter of circle: ",2*3.14*r)    
